089081ad30d01bbf27f6a0df0096f4764c05c561
Steeve Droz <steeve.droz@rpn.ch>
2018-06-18 11:29:12 +0200

    Personalisation de l'URL

---
+++ application/config/portfolio_config.template.php
@@ -0,0 +1,6 @@
+<? php
+// Dupliquez ce fichier dans application/config/portfolio_config.php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['base_url'] = ''; // URL local de la base du site
